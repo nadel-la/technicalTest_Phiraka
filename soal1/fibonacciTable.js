fd18f263b0cc22de87548c9181f76879c572aae1
nadel-la <nadella.ayudhia@gmail.com>
2023-06-22 10:22:04 +0700

    Add: Soal1 done

---
+++ soal1/fibonacciTable.js
@@ -0,0 +1,33 @@
+/* 1. Define the order of the fibonacci*/
+function generateFibonacciList(n) {
+  let number1 = 0;
+  let number2 = 1;
+  let list = [];
+
+  for (let i = 1; i <= n; i++) {
+    list.push(number1);
+    let nextNum = number1 + number2;
+    number1 = number2;
+    number2 = nextNum;
+  }
+  //   console.log(list, "<<");
+  return list;
+}
+
+function fibonacciTable(rows, columns) {
+  let fibonacciLists = generateFibonacciList(rows * columns);
+  let output = "";
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const index = i * columns + j;
+      output += fibonacciLists[index] + " ";
+    }
+    output += "\n";
+  }
+  return output;
+}
+
+let rows = 2;
+let columns = 4;
+console.log(fibonacciTable(rows, columns));
