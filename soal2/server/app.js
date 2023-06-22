298842cb0c3459507f182144d5a76b9116a6ab2c
nadel-la <nadella.ayudhia@gmail.com>
2023-06-22 11:42:31 +0700

    feat: edit user - server

--- soal2/server/app.js
+++ soal2/server/app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 const UserController = require("./controller/userController");
+const { verifyToken } = require("./helpers/jwt");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -11,7 +12,32 @@ app.use(express.json());
 
 app.post("/register", UserController.register);
 app.post("/login", UserController.login);
+
+//Authentication:
+app.use((req, res, next) => {
+  try {
+    let { access_token } = req.headers;
+    if (!access_token) {
+      throw { name: "invalidToken" };
+    }
+    let payload = verifyToken(access_token);
+    if (!payload) {
+      throw { name: "invalidToken" };
+    }
+    req.user = payload;
+    next();
+  } catch (error) {
+    if (error.name === "invalidToken" || error.name === "JsonWebTokenError") {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.get("/", UserController.fetchUsers);
+app.delete("/:id", UserController.deleteUser);
+app.patch("/:id", UserController.editUser);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- soal2/server/controller/userController.js
+++ soal2/server/controller/userController.js
@@ -73,6 +73,59 @@ class UserController {
       res.status(500).json({ message: "Internal server error" });
     }
   }
+
+  static async deleteUser(req, res) {
+    try {
+      let { id } = req.params;
+      let findUser = await User.findByPk(id);
+      if (!findUser) {
+        throw { name: "notFound" };
+      }
+
+      await User.destroy({
+        where: {
+          id,
+        },
+      });
+      res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+      if (error.name === "notFound") {
+        return res.status(404).json({ message: "Data not found" });
+      }
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async editUser(req, res) {
+    try {
+      const { id } = req.params;
+      const { username } = req.body;
+
+      const findUser = await User.findByPk(id);
+      if (!findUser) {
+        throw { name: "notFound" };
+      }
+
+      await User.update(
+        { username },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+      res.status(200).json({
+        msg: `Name updated successfully from ${findUser.username} to ${username}`,
+      });
+    } catch (error) {
+      if (error.name === "notFound") {
+        return res.status(404).json({ message: "Data not found" });
+      }
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
 }
 
 module.exports = UserController;
